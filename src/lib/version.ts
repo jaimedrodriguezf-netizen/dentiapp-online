a562c025721b47dc3ab91da96f4996133eb07766
Jaime Rodriguez <jaimedrodriguezf@gmail.com>
2026-05-17 17:42:39 -0500

    chore: bump to v1.0.2

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.0.1'
-export const APP_NAME = 'DentiApp Online'
-export const APP_DESCRIPTION = 'Panel administrativo para clínicas dentales'
+export const APP_VERSION = "1.0.2"
+export const APP_NAME = "DentiApp Online"
+export const APP_DESCRIPTION = "Panel administrativo para clínicas dentales"
